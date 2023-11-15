ef23b06114b3d13be39cd797bda4bb159713ca73
hc8756 <hc8756@rit.edu>
2023-11-14 21:03:08 -0500

    Enemy player collision improved

--- Untitled Game/Assets/Scripts/EnemySensor.cs
+++ Untitled Game/Assets/Scripts/EnemySensor.cs
@@ -7,7 +7,7 @@ public class EnemySensor : MonoBehaviour
     public bool sensed = false;
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Feet"))
         {
             sensed = true;
         }
